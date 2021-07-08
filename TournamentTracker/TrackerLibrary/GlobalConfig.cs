1f978d77a06a2ab38907657b93dc2a890d122a5f
GabrielGrigore17 <74931518+GabrielGrigore17@users.noreply.github.com>
2021-07-08 16:26:33 +0300

    Started to integrate SQL

--- TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Text;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
@@ -20,9 +22,14 @@ namespace TrackerLibrary
             if (textFiles)
             {
                 // TODO - Create the text file connection
-                TextConnection text = new TextConnection();
+                TextConnector text = new TextConnector();
                 Connections.Add(text);
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
